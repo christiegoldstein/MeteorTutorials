455b9cc11547a6ac50370965757c50b913ef478d
christiegoldstein <cngold15@gmail.com>
2016-09-06 13:13:16 -0400

    import starting code

--- client/main.js
+++ client/main.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
  
-import App from '../imports/ui/App.jsx';
- 
+import '../imports/ui/body.js';
+
 Meteor.startup(() => {
   render(<App />, document.getElementById('render-target'));
 });
